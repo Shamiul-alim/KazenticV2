7814437f529ce095f416f8e9dc97f9b4ddf9f17c
hasib <mdhsbaskari@gmail.com>
2026-01-25 17:06:08 +0600

    feat(dashboard): restore Sidebar and SideMenu components for improved layout structure

--- src/app/(application)/layout.tsx
+++ src/app/(application)/layout.tsx
@@ -9,9 +9,9 @@ const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     <div className="flex flex-col h-screen">
       <Topbar />
       <div className="flex grow">
-        {/* <Sidebar /> */}
-        <div className="bg-[#111953] relative">
-          {/* <SideMenu /> */}
+        <Sidebar />
+        <div className="bg-[#111953] relative hidden lg:flex">
+          <SideMenu />
         </div>
         <div className="w-full bg-[#111953] flex flex-col">
           <ApplicationTopbar />
